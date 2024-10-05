8dc720f91cfa766d073e223747fe363dbe305a61
cem <cemyilmaztech@gmail.com>
2024-10-05 13:42:26 +0300

    login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,6 +23,23 @@ const Navbar = () => {
         >
           HOME
         </Typography>
+        <Typography
+          variant="h6"
+          noWrap
+          component="a"
+          href="#app-bar-with-responsive-menu"
+          sx={{
+            mr: 2,
+            display: { xs: 'none', md: 'flex' },
+            fontFamily: 'monospace',
+            fontWeight: 700,
+            letterSpacing: '.3rem',
+            color: 'inherit',
+            textDecoration: 'none',
+          }}
+        >
+          LOGİN
+        </Typography>
 
        
           
